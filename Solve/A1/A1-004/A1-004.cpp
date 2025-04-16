636566e7af9284ff517f4ba4ac00eb2d6c01d1cd
Nattapat Saisroi <pto65972@gmail.com>
2025-04-17 03:35:31 +0700

    Add files via upload
    Signed-off-by: Nattapat Saisroi <pto65972@gmail.com>

---
+++ Solve/A1/A1-004/A1-004.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a,b,c;
+int main(){
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
+    cin >> a >> b >> c;
+    if (a>=5 && b>=20 && c>=25) cout << "pass";
+    else cout << "fail";
+}
