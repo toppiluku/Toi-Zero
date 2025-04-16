9784aca8b2e2a55b9c9efa38763ef2ba494d937a
Nattapat Saisroi <pto65972@gmail.com>
2025-04-17 02:21:42 +0700

    Add files via upload
    Signed-off-by: Nattapat Saisroi <pto65972@gmail.com>

---
+++ Solve/A1/A1-001/A1-001.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+char a[101],b[101];
+int main(){
+    scanf("%s",&a);
+    scanf("%s",&b);
+    printf("Hello %s %s\n",a,b);
+    printf("%.2s%.2s",a,b);
+}
---
+++ Solve/A1/A1-001/A1-001.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+string c,d;
+int main(){
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
+    cin >> c >> d;
+    cout << "Hello " << c << " " << d << "\n";
+    cout << c[0] << c[1] << d[0] << d[1];
+}
