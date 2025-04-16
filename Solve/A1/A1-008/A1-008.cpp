a642fca25ca2dd01858541ffbb82a844cf65d425
Nattapat Saisroi <pto65972@gmail.com>
2025-04-17 04:43:47 +0700

    Add files via upload
    Signed-off-by: Nattapat Saisroi <pto65972@gmail.com>

---
+++ Solve/A1/A1-008/A1-008.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main(){
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
+    cin >> s;
+    if (s.size()==13) cout << "yes";
+    else cout << "no";
+}
