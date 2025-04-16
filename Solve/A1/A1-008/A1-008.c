c73ec31c8f3df66b156b6a80ce241fb98eb40e39
Nattapat Saisroi <pto65972@gmail.com>
2025-04-17 04:44:19 +0700

    Add files via upload
    Signed-off-by: Nattapat Saisroi <pto65972@gmail.com>

---
+++ Solve/A1/A1-008/A1-008.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+char s[20];
+int main(){
+    scanf("%s",&s);
+    if (strlen(s)==13){
+        printf("yes");
+    }
+    else printf("no");
+}
---
+++ Solve/A1/A1-008/A1-008.py
@@ -0,0 +1,5 @@
+s=input()
+if (len(s) == 13):
+    print("yes")
+else:
+    print("no")
